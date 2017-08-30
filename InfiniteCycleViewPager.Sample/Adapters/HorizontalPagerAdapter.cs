8d73551e6867a062d740470313b38e43074c1df8
Yaroslav Anisimov <anyarik0@gmail.com>
2017-08-30 21:45:20 +0300

    Убрал лишнее в проекте

--- InfiniteCycleViewPager.Sample/Adapters/HorizontalPagerAdapter.cs
+++ InfiniteCycleViewPager.Sample/Adapters/HorizontalPagerAdapter.cs
@@ -4,7 +4,7 @@ using Android.Support.V4.View;
 using Android.Views;
 using Android.Widget;
 using Com.Gigamole.Infinitecycleviewpager;
-using InfiniteCycleViewPager.Sample.Screens;
+
 using InfiniteCycleViewPager.Sample.Utils;
 
 namespace InfiniteCycleViewPager.Sample.Adapters
@@ -38,55 +38,6 @@ namespace InfiniteCycleViewPager.Sample.Adapters
             )
     };
 
-        private Utils.LibraryObject[] TWO_WAY_LIBRARIES = new Utils.LibraryObject[]{
-            new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/AnimateFilterCalendarGridLayout",
-                    new String[]{
-                            "https://lh6.googleusercontent.com/-OMVt5Ow2tsE/VsbvbmJOouI/AAAAAAAACLs/aQoAzX5hGYw/w386-h551-no/afgl.gif",
-                            "https://lh4.googleusercontent.com/-dXoUnGjGLHE/VsbvbtehV3I/AAAAAAAACLs/EqYB6AA5fYI/w396-h552-no/acgl.gif"
-                    }
-            ),
-            new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/SlideImageView",
-                    new String[]{
-                            "https://lh5.googleusercontent.com/-2BiBFz-OW_s/VU_EPv33XMI/AAAAAAAAAcc/nGie_kBoZQ8/w244-h368-no/siv.gif"
-                    }
-            ),
-            new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/MillSpinners",
-                    new String[]{
-                            "https://lh3.googleusercontent.com/-xmtjZYfrZ3g/VR-7WR2tQ1I/AAAAAAAAAZs/YzLSSYPukYE/s0/multi_arc_spinner.gif",
-                            "https://lh5.googleusercontent.com/-PaQXf0zlY5E/VU00AZvffaI/AAAAAAAAAa4/VH36UhvzXl4/w153-h165-no/cws.gif",
-                            "https://lh4.googleusercontent.com/-ysG5WmZKG0w/VU00Ao5_MDI/AAAAAAAAAa0/bYf3v90XpcA/w144-h146-no/cs.gif",
-                            "https://lh5.googleusercontent.com/-X61M2jmXkTs/VU00AdyLk9I/AAAAAAAAAbI/4BKr42uJ4Vc/w170-h261-no/dls.gif",
-                            "https://lh5.googleusercontent.com/-b8PfdN9PoLw/VU00BPNG2cI/AAAAAAAAAbE/AXDZuOMEb5k/w175-h262-no/fs.gif"
-                    }
-            ),
-             new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/AnimateFilterCalendarGridLayout",
-                    new String[]{
-                            "https://lh6.googleusercontent.com/-OMVt5Ow2tsE/VsbvbmJOouI/AAAAAAAACLs/aQoAzX5hGYw/w386-h551-no/afgl.gif",
-                            "https://lh4.googleusercontent.com/-dXoUnGjGLHE/VsbvbtehV3I/AAAAAAAACLs/EqYB6AA5fYI/w396-h552-no/acgl.gif"
-                    }
-            ),
-            new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/SlideImageView",
-                    new String[]{
-                            "https://lh5.googleusercontent.com/-2BiBFz-OW_s/VU_EPv33XMI/AAAAAAAAAcc/nGie_kBoZQ8/w244-h368-no/siv.gif"
-                    }
-            ),
-            new Utils.LibraryObject(
-                    "https://github.com/DevLight-Mobile-Agency/MillSpinners",
-                    new String[]{
-                            "https://lh3.googleusercontent.com/-xmtjZYfrZ3g/VR-7WR2tQ1I/AAAAAAAAAZs/YzLSSYPukYE/s0/multi_arc_spinner.gif",
-                            "https://lh5.googleusercontent.com/-PaQXf0zlY5E/VU00AZvffaI/AAAAAAAAAa4/VH36UhvzXl4/w153-h165-no/cws.gif",
-                            "https://lh4.googleusercontent.com/-ysG5WmZKG0w/VU00Ao5_MDI/AAAAAAAAAa0/bYf3v90XpcA/w144-h146-no/cs.gif",
-                            "https://lh5.googleusercontent.com/-X61M2jmXkTs/VU00AdyLk9I/AAAAAAAAAbI/4BKr42uJ4Vc/w170-h261-no/dls.gif",
-                            "https://lh5.googleusercontent.com/-b8PfdN9PoLw/VU00BPNG2cI/AAAAAAAAAbE/AXDZuOMEb5k/w175-h262-no/fs.gif"
-                    }
-            )
-    };
-
         private Context mContext;
         private LayoutInflater mLayoutInflater;
 
@@ -109,7 +60,7 @@ namespace InfiniteCycleViewPager.Sample.Adapters
         {
             get
             {
-                return mIsTwoWay ? TWO_WAY_LIBRARIES.Length : LIBRARIES.Length;
+                return  LIBRARIES.Length;
             }
         }
 
@@ -120,19 +71,6 @@ namespace InfiniteCycleViewPager.Sample.Adapters
 
         public override Java.Lang.Object InstantiateItem(ViewGroup container, int position)
         {
-            //if (position == 0)
-            //{
-            //    horizontalPagerFragment.backgroundView.Background = horizontalPagerFragment.Resources.GetDrawable(Resource.Drawable.background_one);
-            //}
-            //else if (position == 1)
-            //{
-            //    horizontalPagerFragment.backgroundView.Background = horizontalPagerFragment.Resources.GetDrawable(Resource.Drawable.background_two);
-            //}
-            //else if (position == 2)
-            //{
-            //    horizontalPagerFragment.backgroundView.Background = horizontalPagerFragment.Resources.GetDrawable(Resource.Drawable.background_three);
-            //}
-
             View view;
 
             view = mLayoutInflater.Inflate(Resource.Layout.item, container, false);
